e98398374269e3b029f029d37516068aa13bfd54
Zubrnpc <sol.witek@gmail.com>
2026-04-19 15:19:05 +0200

    Update ZubrNPC.java

--- src/main/java/pl/zubermc/npc/ZubrNPC.java
+++ src/main/java/pl/zubermc/npc/ZubrNPC.java
@@ -1,45 +1,41 @@
 package pl.zubermc.npc;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import net.minecraft.network.protocol.game.*;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.*;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
+import org.bukkit.craftbukkit.v1_21_R4.CraftServer;
+import org.bukkit.craftbukkit.v1_21_R4.CraftWorld;
+import org.bukkit.craftbukkit.v1_21_R4.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.lang.reflect.Field;
+import java.net.URL;
 import java.util.*;
 
 public class ZubrNPC extends JavaPlugin implements Listener {
 
-    private Entity npc;
-    private UUID npcUUID;
-
-    private FileConfiguration config;
-    private File configFile;
+    private ServerPlayer npc;
+    private int npcId;
 
     private String commandName = "npczuber";
-    private String subCreate = "stworz";
-    private String subKill = "usun";
-    private String subCmd = "komenda";
-    private String subMode = "tryb";
-    private String subReload = "reload";
-
     private String clickCommand = "say Witaj %player%!";
     private boolean executeAsConsole = true;
 
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
-        loadConfig();
         registerCommand();
-        getLogger().info("ZubrNPC działa!");
+        getLogger().info("ZubrNPC PRO działa!");
     }
 
     @Override
@@ -47,19 +43,7 @@ public class ZubrNPC extends JavaPlugin implements Listener {
         removeNPC();
     }
 
-    private void loadConfig() {
-        if (!getDataFolder().exists()) getDataFolder().mkdirs();
-        configFile = new File(getDataFolder(), "config.yml");
-        if (!configFile.exists()) saveResource("config.yml", false);
-        config = YamlConfiguration.loadConfiguration(configFile);
-
-        commandName = config.getString("command.name", "npczuber");
-        subCreate = config.getString("command.create", "stworz");
-        subKill = config.getString("command.kill", "usun");
-        subCmd = config.getString("command.setcmd", "komenda");
-        subMode = config.getString("command.mode", "tryb");
-        subReload = config.getString("command.reload", "reload");
-    }
+    // ================= KOMENDA =================
 
     private void registerCommand() {
         try {
@@ -67,82 +51,130 @@ public class ZubrNPC extends JavaPlugin implements Listener {
             f.setAccessible(true);
             CommandMap map = (CommandMap) f.get(Bukkit.getServer());
 
-            if (map.getCommand(commandName) != null) {
-                map.getCommand(commandName).unregister(map);
-            }
+            map.register("zubrnpc", new Command(commandName) {
+                @Override
+                public boolean execute(CommandSender sender, String label, String[] args) {
+                    if (!(sender instanceof Player p)) return false;
 
-            map.register("zubrnpc", new DynamicCommand(commandName));
+                    if (args.length == 0) {
+                        p.sendMessage("/npczuber stworz <nick>");
+                        return true;
+                    }
+
+                    if (args[0].equalsIgnoreCase("stworz")) {
+                        if (args.length < 2) {
+                            p.sendMessage("Podaj nick do skina!");
+                            return true;
+                        }
+
+                        String nick = args[1];
+                        String[] skin = getSkin(nick);
+
+                        if (skin == null) {
+                            p.sendMessage("Nie udało się pobrać skina!");
+                            return true;
+                        }
+
+                        spawnNPC(p.getLocation(), skin[0], skin[1]);
+                        p.sendMessage("NPC ze skinem " + nick + " stworzony!");
+                        return true;
+                    }
+
+                    if (args[0].equalsIgnoreCase("usun")) {
+                        removeNPC();
+                        p.sendMessage("NPC usunięty!");
+                        return true;
+                    }
+
+                    return false;
+                }
+            });
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private class DynamicCommand extends Command {
+    // ================= NPC =================
 
-        protected DynamicCommand(String name) {
-            super(name);
-            this.setPermission("zubr.admin");
-        }
-
-        @Override
-        public boolean execute(CommandSender sender, String label, String[] args) {
-            if (!(sender instanceof Player p)) return false;
-            if (!p.isOp()) return false;
-
-            if (args.length == 0) {
-                p.sendMessage("§e/" + label + " " + subCreate + " §7- tworzy NPC");
-                p.sendMessage("§e/" + label + " " + subKill + " §7- usuwa NPC");
-                p.sendMessage("§e/" + label + " " + subCmd + " <komenda>");
-                return true;
-            }
-
-            if (args[0].equalsIgnoreCase(subCreate)) {
-                spawnNPC(p.getLocation());
-                p.sendMessage("§aNPC stworzony!");
-                return true;
-            }
-
-            if (args[0].equalsIgnoreCase(subKill)) {
-                removeNPC();
-                p.sendMessage("§cNPC usunięty");
-                return true;
-            }
-
-            if (args[0].equalsIgnoreCase(subCmd)) {
-                if (args.length < 2) return false;
-                clickCommand = String.join(" ", Arrays.copyOfRange(args, 1, args.length));
-                p.sendMessage("§aUstawiono komendę!");
-                return true;
-            }
-
-            return false;
-        }
-    }
-
-    private void spawnNPC(Location loc) {
+    private void spawnNPC(Location loc, String texture, String signature) {
         removeNPC();
 
-        npc = loc.getWorld().spawnEntity(loc, EntityType.VILLAGER);
-        npc.setCustomName("§6§lŻubr Przewodnik");
-        npc.setCustomNameVisible(true);
-        npc.setInvulnerable(true);
-        npc.setSilent(true);
+        var server = ((CraftServer) Bukkit.getServer()).getServer();
+        var world = ((CraftWorld) loc.getWorld()).getHandle();
 
-        npcUUID = npc.getUniqueId();
+        GameProfile profile = new GameProfile(UUID.randomUUID(), "§6§lŻubr");
+
+        profile.getProperties().put("textures", new Property("textures", texture, signature));
+
+        npc = new ServerPlayer(server, world, profile);
+
+        npc.setPos(loc.getX(), loc.getY(), loc.getZ());
+        npc.setYRot(loc.getYaw());
+        npc.setXRot(loc.getPitch());
+
+        npcId = npc.getId();
+
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            ServerGamePacketListenerImpl conn = ((CraftPlayer) p).getHandle().connection;
+
+            conn.send(new ClientboundPlayerInfoUpdatePacket(
+                    ClientboundPlayerInfoUpdatePacket.Action.ADD_PLAYER, npc));
+
+            conn.send(new ClientboundAddEntityPacket(npc));
+
+            conn.send(new ClientboundRotateHeadPacket(npc,
+                    (byte) ((int) (loc.getYaw() * 256 / 360))));
+
+            Bukkit.getScheduler().runTaskLater(this, () ->
+                    conn.send(new ClientboundPlayerInfoRemovePacket(List.of(npc.getUUID()))), 40L);
+        }
     }
 
     private void removeNPC() {
-        if (npc != null) {
-            npc.remove();
-            npc = null;
-            npcUUID = null;
+        if (npc == null) return;
+
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            ServerGamePacketListenerImpl conn = ((CraftPlayer) p).getHandle().connection;
+            conn.send(new ClientboundRemoveEntitiesPacket(npcId));
         }
+
+        npc = null;
     }
+
+    // ================= SKIN =================
+
+    private String[] getSkin(String nick) {
+        try {
+            URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + nick);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String response = reader.readLine();
+
+            if (response == null) return null;
+
+            String uuid = response.split("\"id\":\"")[1].split("\"")[0];
+
+            URL url2 = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid + "?unsigned=false");
+            BufferedReader reader2 = new BufferedReader(new InputStreamReader(url2.openStream()));
+            String json = reader2.lines().reduce("", String::concat);
+
+            String value = json.split("\"value\":\"")[1].split("\"")[0];
+            String sig = json.split("\"signature\":\"")[1].split("\"")[0];
+
+            return new String[]{value, sig};
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // ================= KLIK =================
 
     @EventHandler
     public void onClick(PlayerInteractEntityEvent e) {
-        if (npcUUID == null) return;
-        if (!e.getRightClicked().getUniqueId().equals(npcUUID)) return;
+        if (npc == null) return;
+        if (e.getRightClicked().getEntityId() != npcId) return;
 
         e.setCancelled(true);
 
